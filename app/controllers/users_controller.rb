e85a0a2f0c51df638769a3bacacbf61fc9f5ac62
MarcusMellorMIM <marcus@mellor.com>
2019-07-04 15:47:44 +0100

    Created user routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+    def show
+        user = User.where(["user_name=?",params[:id]])
+        render json: user
+    end
+
+    def create
+        user = User.create user_params
+        render json: user
+    end
+
+    def update
+        user.assign_attributes(user_params)
+        if user.valid?
+            user.save
+        end
+        render json: user
+    end
+
+private
+
+    def user_params
+        params.require(:user).permit(:password_digest,
+                    :email,
+                    :name,
+                    :dob,
+                    :height_cm,
+                    :gender)
+        end
+
+    end
